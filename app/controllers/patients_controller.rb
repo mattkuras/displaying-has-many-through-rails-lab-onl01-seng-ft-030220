ce568bf3a8d95e55c3946e9c9ec7d17f32582ebf
matt_kuras <mattkuras@gmail.com>
2020-05-18 19:00:02 -0400

    Done.

---
+++ app/controllers/patients_controller.rb
@@ -0,0 +1,9 @@
+class PatientsController < ApplicationController
+    def index 
+        @patients = Patient.all
+    end
+
+    def show 
+        @patient = Patient.all.find(params[:id])
+    end
+end
